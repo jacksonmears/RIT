d3462086ecc3b729746c3b84961909a85b7a34bb
Jackson Mears <jackson.mears2002@gmail.com>
2025-03-08 16:38:35 -0600

    committ #8
    added displayName collection that ensures that all displayNames are different allowing us to properly search for friends which is the next task (search, sending, and accepting friend invites).

---
+++ app/(tabs)/home/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+
+const Layout = () => {
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" /> {/* Handles the account home page */}
+            <Stack.Screen name="createAccount" /> {/* Handles the display name change */}
+        </Stack>
+    );
+};
+
+export default Layout;
