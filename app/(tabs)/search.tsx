f8225db3267bde2894900fba5029e4500ac9cddc
Jackson Mears <jackson.mears2002@gmail.com>
2025-03-08 23:42:20 -0600

    committ #9
    can now send friend requests and they are stored in firebase firestore!

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,14 +1,47 @@
 import {View, Text, Button, Pressable, TextInput, StyleSheet} from 'react-native';
-import { auth } from '@/firebase';
-import { useState } from 'react';
+import {auth, db} from '@/firebase';
+import { useState, useEffect } from 'react';
+import {doc, getDoc, setDoc } from "firebase/firestore";
 
 const Page = () => {
     const user = auth.currentUser;
     const [search, setSearch] = useState('');
+    const [found, setFound] = useState('');
+
 
     const handleSearch = async () => {
-
+        setFound('');
+        if (search == user?.displayName){
+            return;
+        } else {
+            const docRef = doc(db, "displayName", search);
+            const docSnap = await getDoc(docRef);
+            docSnap.exists() ? setFound(docSnap.data().uid) : setFound('');
+            console.log(docSnap.data());
+        }
     }
+
+
+    const sendRequest = async () => {
+        if (user){
+            const docRef = doc(db, "users", found);
+            const docSnap = await getDoc(docRef);
+            const requests = docSnap.data()?.requests || [];
+
+            if (requests.includes(user.uid)){
+                console.log("Friend request already sent!");
+                return;
+            }
+
+            if (docSnap.exists()) {
+                await setDoc(docRef, { requests: [...requests, user.uid] }, { merge: true });
+            } else {
+                await setDoc(docRef, { requests: [user.uid] });
+            }
+        }
+    }
+
+
 
     return (
         <View style={styles.container}>
@@ -18,11 +51,16 @@ const Page = () => {
                 placeholder="friends name"
                 placeholderTextColor="#ccc"
                 value={search}
-                onChangeText={setSearch} // Updates state
+                onChangeText={setSearch}
             />
             <Pressable onPress={handleSearch} style={styles.button}>
                 <Text> Search for friends </Text>
             </Pressable>
+
+            <Pressable onPress={sendRequest} style={styles.reqButton}>
+                <Text> add {found} </Text>
+            </Pressable>
+
         </View>
     )
 }
@@ -53,6 +91,15 @@ const styles = StyleSheet.create({
         backgroundColor: "white",
         borderRadius: 4,
         top: 50,
+        left: 50,
+    },
+    reqButton: {
+        marginVertical: 4,
+        marginHorizontal: 40,
+        position: "absolute",
+        backgroundColor: "white",
+        borderRadius: 4,
+        top: 500,
         left: 50,
     }
 })
