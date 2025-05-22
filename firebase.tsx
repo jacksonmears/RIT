891f2af9e217d7a833efe04c85e86531feba6ea2
Jackson Mears <jackson.mears2002@gmail.com>
2025-05-21 22:05:52 -0500

    commit #13
    updating files for eas build

--- firebase.tsx
+++ firebase.tsx
@@ -11,9 +11,9 @@
 //
 // const firebaseConfig = {
 //     apiKey: "AIzaSyBl82zHQla3xSfooZ0cc49YfA5W4MdTTBQ",
-//     authDomain: "recap-d22e0.firebaseapp.com",
-//     projectId: "recap-d22e0",
-//     storageBucket: "recap-d22e0.firebasestorage.app",
+//     authDomain: "rit-d22e0.firebaseapp.com",
+//     projectId: "rit-d22e0",
+//     storageBucket: "rit-d22e0.firebasestorage.app",
 //     messagingSenderId: "530663415978",
 //     appId: "1:530663415978:android:be4aedd13844ee9728b0d3",
 // };
@@ -45,9 +45,9 @@
 //
 // const firebaseConfig = {
 //     apiKey: "AIzaSyBl82zHQla3xSfooZ0cc49YfA5W4MdTTBQ",
-//     authDomain: "recap-d22e0.firebaseapp.com",
-//     projectId: "recap-d22e0",
-//     storageBucket: "recap-d22e0.appspot.com",  // ❗ Fixed this too
+//     authDomain: "rit-d22e0.firebaseapp.com",
+//     projectId: "rit-d22e0",
+//     storageBucket: "rit-d22e0.appspot.com",  // ❗ Fixed this too
 //     messagingSenderId: "530663415978",
 //     appId: "1:530663415978:android:be4aedd13844ee9728b0d3",
 // };
@@ -84,9 +84,9 @@
 // // Your new Firebase project configuration
 // const firebaseConfig = {
 //     apiKey: "AIzaSyBl82zHQla3xSfooZ0cc49YfA5W4MdTTBQ",
-//     authDomain: "recap-d22e0.firebaseapp.com", // typically projectId.firebaseapp.com
-//     projectId: "recap-d22e0",
-//     storageBucket: "recap-d22e0.firebasestorage.app",
+//     authDomain: "rit-d22e0.firebaseapp.com", // typically projectId.firebaseapp.com
+//     projectId: "rit-d22e0",
+//     storageBucket: "rit-d22e0.firebasestorage.app",
 //     messagingSenderId: "530663415978",
 //     appId: "1:530663415978:android:be4aedd13844ee9728b0d3",
 // };
@@ -117,9 +117,9 @@
 // // Your Firebase project configuration
 // const firebaseConfig = {
 //     apiKey: "AIzaSyBl82zHQla3xSfooZ0cc49YfA5W4MdTTBQ",
-//     authDomain: "recap-d22e0.firebaseapp.com",
-//     projectId: "recap-d22e0",
-//     storageBucket: "recap-d22e0.firebasestorage.app",
+//     authDomain: "rit-d22e0.firebaseapp.com",
+//     projectId: "rit-d22e0",
+//     storageBucket: "rit-d22e0.firebasestorage.app",
 //     messagingSenderId: "530663415978",
 //     appId: "1:530663415978:android:be4aedd13844ee9728b0d3",
 // };
@@ -144,9 +144,9 @@ import storage from '@react-native-firebase/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBl82zHQla3xSfooZ0cc49YfA5W4MdTTBQ",
-    authDomain: "recap-d22e0.firebaseapp.com",
-    projectId: "recap-d22e0",
-    storageBucket: "recap-d22e0.firebasestorage.app",
+    authDomain: "rit-d22e0.firebaseapp.com",
+    projectId: "rit-d22e0",
+    storageBucket: "rit-d22e0.firebasestorage.app",
     messagingSenderId: "530663415978",
     appId: "1:530663415978:android:be4aedd13844ee9728b0d3",
 };
